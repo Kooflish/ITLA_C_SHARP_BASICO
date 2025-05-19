60f2207d4199b0b3c734b6502b8b768cdddc5a95
Kooflish <kgonzalo9812@gmail.com>
2025-05-19 16:38:50 -0400

    Add project files.

---
+++ Dividirnumeros/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class ProgramaDivision
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Introduce el primer número: ");
+            double numero1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Introduce el segundo número: ");
+            double numero2 = Convert.ToDouble(Console.ReadLine());
+
+            if (numero2 == 0)
+            {
+                Console.WriteLine("Error: No se puede dividir entre cero.");
+            }
+            else
+            {
+                double resultado = numero1 / numero2;
+                Console.WriteLine("Resultado de la división: " + resultado);
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: Debes introducir números válidos.");
+        }
+    }
+}
