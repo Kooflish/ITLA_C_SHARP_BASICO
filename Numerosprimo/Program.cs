a840d054d16b521458a870680b188ff08aff793d
Kooflish <kgonzalo9812@gmail.com>
2025-05-20 11:08:11 -0400

    EJERCICIO 04 LISTO

---
+++ Numerosprimo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroPrimo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-Numeros Primo-");
+
+            Console.Write("Ingrese un número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (EsPrimo(numero))
+            {
+                Console.WriteLine($"{numero} es un número primo.");
+            }
+            else
+            {
+                Console.WriteLine($"{numero} NO es un número primo.");
+            }
+
+            Console.WriteLine("\nPresione cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+
+        static bool EsPrimo(int n)
+        {
+            if (n <= 1)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
