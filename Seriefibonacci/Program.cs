72ed97747b023564479c8b625caa55f6abece915
Kooflish <kgonzalo9812@gmail.com>
2025-05-20 11:25:18 -0400

    EJERCICIO 06 LISTO

---
+++ Seriefibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 0, b = 1, temp;
+
+        Console.WriteLine("Serie Fibonacci del 1 al 50:");
+
+        while (b <= 50)
+        {
+            Console.WriteLine(b);
+            temp = a + b;
+            a = b;
+            b = temp;
+        }
+    }
+}
