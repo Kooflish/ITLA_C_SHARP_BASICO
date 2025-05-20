93e993a00ae7100a3d34a67b07f8716e53525ec5
Kooflish <kgonzalo9812@gmail.com>
2025-05-20 11:21:36 -0400

    EJERCICIO 05 LISTO

---
+++ Numerosimpares/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Números impares del 1 al 30:");
+
+        for (int i = 1; i <= 30; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
